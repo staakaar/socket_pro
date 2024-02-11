ac6a91950c808e83d1f33ebe654532324c0c985f
staakaar <vancyroletaka@gmail.com>
2024-02-12 01:04:19 +0900

    add packet-capture project

---
+++ packet-capture/src/main.rs
@@ -0,0 +1,48 @@
+use log::{error, info};
+use pnet::{datalink::{self, EtherType}, packet::{ipv4, ipv6}};
+
+fn main() {
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+    let args: Vec<String> = env::args.collect();
+    if args.len() != 2 {
+        error!("Please specify target interface name");
+        std::process::exit(1);
+    }
+
+    let interface_name = &args[1];
+
+    let interfaces = datalink::interfaces();
+    let interface = interfaces.into_iter().find(|iface| iface.name == *interface_name).expect("Failed to get interface");
+
+    /** データリンクのチャンネルを取得 */
+    let (_tx, mut rx) = match  datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, tc)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channel type"),
+        Err(e) => panic!("Failed to create datalink channel {}", e),
+    };
+
+    loop {
+        match rx.next() {
+            Ok(frame) => {
+                let frame = EthernetPacket::new(frame).unwrap();
+                match frame.get_ethertype() {
+                    EtherTypes::Ipv4 => {
+                        ipv4_handler(&frame);
+                    }
+                    EtherTypes::Ipv6 => {
+                        ipv6_handler(&frame);
+                    }
+                    _ => {
+                        info!("Not an Ipv4 or Ipv6 packet");
+                    }
+                }
+            }
+            Err(e) => {
+                error!("Failed to read: {}", e);
+            }
+        }
+    }
+}
+
+/** Ipv4パケットを構築次のレイヤーのハンドラを呼び出す */
