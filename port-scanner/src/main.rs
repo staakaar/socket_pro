d6602d1e9cc263857d22d8e1586c5f2358574395
staakaar <vancyroletaka@gmail.com>
2024-02-14 00:52:31 +0900

    initial commit

---
+++ port-scanner/src/main.rs
@@ -0,0 +1,60 @@
+struct PacketInfo {
+    my_ipaddr: Ipv4Addr,
+    target_ipaddr: Ipv4Addr,
+    my_port = u16,
+    maximum_port: u16,
+    scan_type: ScanType,
+}
+
+#[derive(Copy, Clone)]
+enum ScanType {
+    Syn = TcpFlags::AYN as isize,
+    Fin = TcpFlags::FIN as isize,
+    Xmas = (TcpFlags::FIN | TcpFlags::URG | TcpFlags::PSH) as isize,
+    Null = 0,
+}
+
+fn main() {
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        error!("Bad number of arguments. [ipaddr] [scantype]");
+        process::exit(1);
+    }
+
+    let packet_info = {
+        let contents = fs::read_to_string(".env").expect("Failed to read env file");
+        let lines: Vec<_> = contents.split('\n').collect();
+        let mut map = HashMap::new();
+
+        for line in lines {
+            let elm: Vec<_> = line.split('='),map(str::trim).collect();
+
+            if elm.len() == 2 {
+                map.insert(elm[0], elm[1]);
+            }
+        }
+
+        PacketInfo {
+            my_ipaddr: map["MY_APADDR"].parse().expect("invalid ipaddr"),
+            target_ipaddr: args[1].parse().expect("invalid target ipaddr"),
+            my_port: map["MY_PORT"].parse().expect("invalid port number"),
+            maximum_port: map["MAXIMUM_PORT_NUM"].parse().expect("invalid maximum port num"),
+            scan_type: match args[2].as_str() {
+                "sS" => ScanType::Syn,
+                "sF" => ScanType::Fin,
+                "sX" => ScanType::Xmas,
+                "sN" => ScanType::Null,
+                _ => {
+                    error!("Undefined scan method, only accept [sS|sF|sN|sX]");
+                    process::exit(1);
+                }
+            },
+        }
+    };
+
+    //トランスポート層のチャンネル開く
+    let (mut ts, mut tr) = transport::transport_channel(1024, TransportChannelType::Layer4(TransportProtocol::Ipv4(IpNextHeaderProtocols::Tcp))).expect("Failed to open channel")
+}
