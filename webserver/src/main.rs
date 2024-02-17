8c1dc3677d852e0da1b5a242b58b251f1013ff76
staakaar <vancyroletaka@gmail.com>
2024-02-18 00:55:22 +0900

    add http_handler

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,4 +1,6 @@
+use log::error;
 const SERVER: Token = Token(0);
+const WEBROOT: &str = "/webroot";
 
 struct WebServer {
     listening_socket: TcpListener,
@@ -70,9 +72,102 @@ impl WebServer {
         self.next_connection_id += 1;
         Ok(())
     }
+
+    fn http_handler(&mut self, conn_id: usize, event: Event, poll: &Poll, response: &mut Vec<u8>) -> Result<(), failure::Error> {
+        let stream = self.connections.get_mut(&conn_id).ok_or_else(|| failure::err_msg("Failed to get connnection."))?;
+        if event.readiness().is_readable() {
+            debug!("readable conn_id: {}", conn_id);
+            let mut buffer = [0u8, 1024];
+            let nbytes = stream.read(&mut buffer)?;
+
+            if nbytes != 0 {
+                *response = make_response(&buffer[..nbytes])?;
+                poll.reregister(stream, Token(conn_id), Ready::writable, PollOpt::edge())?;
+            } else {
+                self.connections.remove(&conn_id);
+            }
+            Ok(())
+        } else if event.readiness().is_writable() {
+            debug!("writable conn_id: {}", conn_id);
+            stream.write_all(response)?;
+            self.connections.remove(&conn_id);
+            Ok(())
+        } else {
+            Err(failure::err_msg("Undefined event"))
+        }
+    }
+
+    fn make_response(buffer: &[u8]) -> Result<Vec<u8>, failure::Error> {
+        let http_pattern = Regex::new(r"(.*)(.*) HTTP/1.([0-1])\r\n*")?;
+        let captures = match http_pattern.captures(str::from_utf8(buffer)?) {
+            Some(cap) => cap,
+            None => {
+                return create_msg_from_code(400, None);
+            }
+        };
+
+        let method = captures[1].to_string();
+        let path = format!(
+            "{}{}{}",
+            env::current_dir()?.display(),
+            WEBROOT,
+            &captures[2]
+        );
+
+        let _version = captures[3].to_string();
+
+        if methos == "GET" {
+            let file = match File::open(path) {
+                Ok(file) => file,
+                Err(_) => {
+                    return create_msg_from_code(404, None);
+                }
+            }
+            let mut reader = BufReader::new(file);
+            let mut buf - Vec::new();
+            reader.read_to_end(&mut buf)?;
+            create_msg_from_code(200, Some(buf))
+        } else {
+            create_msg_from_code(501, None)
+        }
+    }
+
+    fn create_msg_from_code(status_code: u16, msg: Option<Vec<u8>>) -> Result<Vec<u8>, failure::Error> {
+        match status_code {
+            200 => {
+                let mut header = "HTTP/1.0 200 OK\r\n\ Server: mio webserver\r\n\r\n".to_string().into_bytes();
+
+                if let Some(mut msg) = msg {
+                    header.append(&mut msg);
+                }
+                Ok(header)
+            }
+            400 => Ok("HTTP/1.0 400 Bad Request\r\n\ Server: mio webserver\r\n\r\n".to_string().into_bytes();)
+            404 => Ok("HTTP/1.0 404 Not Found\r\n\ Server: mio webserver\r\n\r\n".to_string().into_bytes();)
+            501 => Ok("HTTP/1.0 501 Not Implemented\r\n\ Server: mio webserver\r\n\r\n".to_string().into_bytes();)
+            _ => Err(failure::err_msg("Undefined status code."))
+        }
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        error!("wrong number of arguments");
+        process::exit(1);
+    }
+
+    let mut server = WebServer::new(&args[1]).unwrap_or_else(|e| {
+        error!("{}", e);
+        panic!();
+    });
+
+    server.run().unwrap_or_else(|e| {
+        error!("{}", e);
+        panic!();
+    });
 }
 
