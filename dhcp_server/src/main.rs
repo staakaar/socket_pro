d54cab0115be7e03a5dd9c9a649b6f88e3972430
staakaar <vancyroletaka@gmail.com>
2024-02-20 00:10:05 +0900

    add impl dhcp_handler

--- dhcp_server/src/main.rs
+++ dhcp_server/src/main.rs
@@ -7,5 +7,30 @@ fn main() {
 
     let dhcp_server = Arc::new(DhcpServer::new().unwrap_or_else(|e| panic!("Failed to start dhcp server. {:?\}", e)));
 
-    loop {}
+    loop {
+        let mut recv_buf = [0u8; 1024];
+        match server_socket.rec_from(&mut recv_buf) {
+            Ok((size, src)) => {
+                debug!("received data from {}, size: {}", src, size);
+                let transmission_socket = server_socket.try_clone().expect("Failed to create client socket");
+
+                let cloned_dhcp_server = dhcp_server.clone();
+
+                thread::spawn(move || {
+                    if Some(dhcp_server) = DhcpPacket::new(recv_buf[..size].to_vec()) {
+
+                        if dhcp_packet.get_op() != BOOTREQUEST {
+                            return;
+                        }
+                        if let Err(e) = dhcp_handler(&dhcp_packet, &transmission_socket, cloned_dhcp_server) {
+                            error!("{}", e);
+                        }
+                    }
+                });
+            }
+            Err(e) => {
+                error!("Cound not receive a datagram: {}", e);
+            }
+        }
+    }
 }
